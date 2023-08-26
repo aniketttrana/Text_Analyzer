5bfaf275731246ea9f88b7b595c671bc073d04a4
anikettrana <anikettrana@gmail.com>
2023-08-26 21:45:48 +0530

    styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar(props) {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand " href="/">
-      {/* /=# */}
-     {props.title}
-    </a> 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">
-           {props.Home}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand " href="/">
+            {/* /=# */}
+            {props.title}
           </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                {/* <a className="nav-link active" aria-current="page" href="/">
+                  {props.Home}
+                </a> */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
