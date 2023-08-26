99c2cabe33c686bbfd645f44404e3d5768335fcb
anikettrana <anikettrana@gmail.com>
2023-08-26 22:25:41 +0530

    fixing

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 
 export default function Alert(props) {
   return (
     // Alert in bootstrap
-   props.alert && <div><div className="alert alert-info {props.alert.type}" role="alert">
-   <strong>{props.alert.type}</strong>:{props.alert.msg}
-  </div></div>
-  )
+    <div style={{ height: "50px" }}>
+      {props.alert && 
+        <div className="alert alert-info {props.alert.type}" role="alert">
+          <strong>{props.alert.type}</strong>:{props.alert.msg}
+       
+      </div>}
+    </div>
+  );
 }
